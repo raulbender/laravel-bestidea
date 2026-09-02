f1365843cea46f032f5d3ee2ccc5fc5379e029e5
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-02 17:08:10 -0300

    feat(models): create RoomUser model with author relationship and migration for room_users table

---
+++ app/Models/RoomUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use app\Models\Author;
+
+class RoomUser extends Model
+{
+    //
+    public function author()
+{
+    return $this->belongsTo(Author::class);
+}
+}
