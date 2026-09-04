1d2fc3c47d31bf6373fc877f9b171c0eb46fd10e
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-04 12:22:13 -0300

    feat(ratings): add RatingFactory for generating rating test data

---
+++ database/factories/RatingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use App\Models\Idea;
+use App\Models\Rating;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RatingFactory extends Factory
+{
+    protected $model = Rating::class;
+
+    public function definition(): array
+    {
+        return [
+            'idea_id'   => Idea::factory(),
+            'user_id'   => User::factory(),
+            'author_id' => Author::factory(),
+            'score'     => $this->faker->numberBetween(1, 5),
+            'feedback'  => $this->faker->sentence(),
+        ];
+    }
+}
