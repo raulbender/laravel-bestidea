d0ec6d3ee7a18fe2ca3dcf74c1fb379d59e189fb
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-05 12:22:34 -0300

    fix(author): correct casing for HasFactory trait usage

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Author extends Model {
-   // use hasfactory;
+    use hasfactory;
 
     protected $guarded = [];
     
