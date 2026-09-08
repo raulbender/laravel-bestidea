4c94442a3e7fce9d79bb951ecfb08399ae6d4cda
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-08 17:58:53 -0300

    feat(i18n): append SetLocale middleware to application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use App\Http\Middleware\EnsureAuthenticatedOrGuest;
+use App\Http\Middleware\SetLocale;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(SetLocale::class);
+
         $middleware->alias([
             'ensure.guest' => EnsureAuthenticatedOrGuest::class,
         ]);
