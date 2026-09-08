08dd0c28e1cef0a5baaa8a12c69fa7f79c040556
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-08 17:58:29 -0300

    feat(i18n): update fallback locale to Portuguese (pt_BR)

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'pt_BR'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'pt_BR'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
