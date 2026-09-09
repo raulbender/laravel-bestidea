2b40ad73908b7bd1552d4736a90ddebed7de0d1e
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-09-09 18:24:02 -0300

    feat(comment): add author_name and created_at_human fields to CommentResource

--- app/Http/Resources/Api/CommentResource.php
+++ app/Http/Resources/Api/CommentResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\CarbonInterface;
 
 class CommentResource extends JsonResource
 {
@@ -15,10 +16,14 @@ class CommentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
-            'content'    => $this->content,
-            'author'     => new AuthorResource($this->whenLoaded('author', $this->author)),
-            'created_at' => $this->created_at,
+            'id'               => $this->id,
+            'content'          => $this->content,
+            'author'           => new AuthorResource($this->whenLoaded('author', $this->author)),
+            'author_name'      => $this->relationLoaded('author') && $this->author 
+                                    ? __($this->author->name) 
+                                    : __('app.idea.anonymous'),
+            'created_at'       => $this->created_at,
+            'created_at_human' => $this->created_at?->diffForHumans(),
         ];
     }
 }
